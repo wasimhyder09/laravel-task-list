a831006285ce6fc36f086c612ab83a9142053fd3
Wasim Hyder <wasim@creativefaze.com>
2024-09-20 15:27:41 +0500

    Added pagination

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -10,4 +10,9 @@
     @empty
       <div>No tasks</div>
     @endforelse
+  @if($tasks->count())
+    <nav>
+      {{$tasks->links()}}
+    </nav>
+  @endif
 @endsection
--- routes/web.php
+++ routes/web.php
@@ -10,7 +10,7 @@ Route::get('/', function () {
 });
 
 Route::get('/tasks', function () {
-  $tasks = Task::latest()->get();
+  $tasks = Task::latest()->paginate(10);
   return view('index', [
     'tasks' => $tasks
   ]);
