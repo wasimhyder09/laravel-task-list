c14aaf4a157e329daaa1134ec05f0aa392f9b826
Wasim Hyder <wasim@creativefaze.com>
2024-09-20 16:07:06 +0500

    Started adding tailwind based designing

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -7,9 +7,10 @@
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
   <title>Laravel 11 Task List App</title>
   @yield('styles')
+  <script src="https://cdn.tailwindcss.com"></script>
 </head>
-<body>
-  <h1 class="title">@yield('title')</h1>
+<body class="container mx-auto mt-10 mb-10 max-w-lg">
+  <h1 class="text-2xl mb-4">@yield('title')</h1>
 <div class="content">
   @if(session()->has('success'))
   <div>
